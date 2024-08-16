aba6031c137365a2764b651c4f271b7a64b06e59
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-08-16 09:26:55 +0530

    chore: updated withAuth and withNoAuth files

--- src/utils/withAuth.tsx
+++ src/utils/withAuth.tsx
@@ -9,41 +9,39 @@ interface UserProps {
   user: User
 }
 
+interface WithAuthOptions {
+  redirectPath: string
+}
+
 /**
- * Higher-order component to restrict access to unauthenticated users.
+ * Higher-order component to restrict access to authenticated users.
  * If a user is not authenticated, they will be redirected to the specified path.
  * If the user is authenticated, they can access the wrapped component.
  *
  * @template P - Props type of the wrapped component
  * @param {ComponentType<P & UserProps>} WrappedComponent - The component to wrap, which requires user authentication
- * @param {string} [redirectPath='/sign-in'] - The path to redirect to if the user is not authenticated (defaults to '/sign-in')
+ * @param {WithAuthOptions} [options] - Options to configure the HOC (e.g., redirect path)
  * @returns {ComponentType<P>} - The wrapped component with authentication logic applied
  *
  * @example
  * ```tsx
- * const ProtectedPage = withAuth(MyPageComponent, '/custom-redirect');
+ * const ProtectedPage = withAuth(MyPageComponent, { redirectPath: '/custom-redirect' })
  * ```
  */
 const withAuth = <P extends object>(
   WrappedComponent: ComponentType<P & UserProps>,
-  redirectPath: string = '/sign-in',
+  options: WithAuthOptions = { redirectPath: '/sign-in' },
 ): ComponentType<P> => {
   const ComponentWithAuth = async (props: P): Promise<ReactElement | null> => {
-    try {
-      const headersList = headers()
-      const user = await getCurrentUser(headersList)
+    const headersList = headers()
+    const user = await getCurrentUser(headersList)
 
-      if (!user) {
-        redirect(redirectPath)
-        return null // Ensure no component is rendered if redirect is triggered
-      }
-
-      return <WrappedComponent user={user} {...props} />
-    } catch (error) {
-      console.error('Error in authentication check:', error)
-      // Optionally handle the error more gracefully
-      return null // Optionally render a fallback UI or redirect on error
+    if (!user) {
+      redirect(options.redirectPath)
+      return null
     }
+
+    return <WrappedComponent user={user} {...props} />
   }
 
   return ComponentWithAuth as ComponentType<P>
--- src/utils/withNoAuth.tsx
+++ src/utils/withNoAuth.tsx
@@ -1,46 +1,49 @@
+import { User } from '@payload-types'
 import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
 import { ComponentType, ReactElement } from 'react'
 
 import { getCurrentUser } from '@/utils/getCurrentUser'
 
+interface UserProps {
+  user: User
+}
+
+interface WithNoAuthOptions {
+  redirectPath: string
+}
+
 /**
  * Higher-order component to restrict access to authenticated users.
- * If the user is authenticated, they will be redirected to the specified path.
- * If the user is not authenticated, they can access the wrapped component.
+ * If a user is not authenticated, they will be redirected to the specified path.
+ * If the user is authenticated, they can access the wrapped component.
  *
  * @template P - Props type of the wrapped component
- * @param {ComponentType<P>} WrappedComponent - The component to wrap
- * @param {string} [redirectPath='/dashboard'] - The path to redirect to if the user is authenticated (defaults to '/dashboard')
- * @returns {ComponentType<P>} - The wrapped component with authentication restriction logic
+ * @param {ComponentType<P & UserProps>} WrappedComponent - The component to wrap, which requires user authentication
+ * @param {WithNoAuthOptions} [options] - Options to configure the HOC (e.g., redirect path)
+ * @returns {ComponentType<P>} - The wrapped component with authentication logic applied
  *
  * @example
  * ```tsx
- * const NonProtectedPage = withNoAuth(MyPageComponent, '/custom-redirect');
+ * const ProtectedPage = withNoAuth(MyPageComponent, { redirectPath: '/custom-redirect' })
  * ```
  */
 const withNoAuth = <P extends object>(
-  WrappedComponent: ComponentType<P>,
-  redirectPath: string = '/dashboard',
+  WrappedComponent: ComponentType<P & UserProps>,
+  options: WithNoAuthOptions = { redirectPath: '/dashboard' },
 ): ComponentType<P> => {
   const ComponentWithNoAuth = async (
     props: P,
   ): Promise<ReactElement | null> => {
-    try {
-      const headersList = headers()
-      const user = await getCurrentUser(headersList)
+    const headersList = headers()
+    const user = await getCurrentUser(headersList)
 
-      if (user) {
-        redirect(redirectPath)
-        return null // Ensure no component is rendered if redirect is triggered
-      }
-
-      return <WrappedComponent {...props} />
-    } catch (error) {
-      console.error('Error in authentication check:', error)
-      // Optionally render fallback UI or handle the error in another way
+    if (user) {
+      redirect(options.redirectPath)
       return null
     }
+
+    return <WrappedComponent user={user} {...props} />
   }
 
   return ComponentWithNoAuth as ComponentType<P>
