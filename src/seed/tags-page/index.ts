6ef4600b57c482c40ea6f93c104057479421f8b0
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-08-19 12:05:03 +0530

    feat: add seeding process for tags page

---
+++ src/seed/tags-page/index.ts
@@ -0,0 +1,7 @@
+// Exporting types and constants from data.ts
+export { tagsPageData } from './data'
+export type { TagsPageDataType } from './data'
+
+// Exporting types and functions from seed.ts
+export { default as seedTagsPage } from './seed'
+export type { Args as SeedTagsPageArgs } from './seed'
