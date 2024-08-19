5e65e5d4d3993956ca07c46e7ca158b8ada4f6ad
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-08-19 12:03:44 +0530

    feat: add seeding process for home page

---
+++ src/seed/home-page/index.ts
@@ -0,0 +1,7 @@
+// Exporting types and constants from data.ts
+export { homePageData } from './data'
+export type { HomePageDataType } from './data'
+
+// Exporting types and functions from seed.ts
+export { default as seedHomePage } from './seed'
+export type { Args as SeedHomePageArgs } from './seed'
