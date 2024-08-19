7655b631046083612041c78e8b29fe79093d42e2
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-08-19 12:03:59 +0530

    feat: add seeding process for tag details page

---
+++ src/seed/tag-details-page/index.ts
@@ -0,0 +1,7 @@
+// Exporting types and constants from data.ts
+export { tagDetailsPageData } from './data'
+export type { TagDetailsPageDataType } from './data'
+
+// Exporting types and functions from seed.ts
+export { default as seedTagDetailsPage } from './seed'
+export type { Args as SeedTagDetailsPageArgs } from './seed'
