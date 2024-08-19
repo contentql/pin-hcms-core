a50718e39e7711dff3f17b16b48affa608238485
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-08-19 12:02:21 +0530

    feat: add seeding process for blog details page

---
+++ src/seed/blog-details-page/index.ts
@@ -0,0 +1,7 @@
+// Exporting types and constants from data.ts
+export { blogDetailsPageData } from './data'
+export type { BlogDetailsPageDataType } from './data'
+
+// Exporting types and functions from seed.ts
+export { default as seedBlogDetailsPage } from './seed'
+export type { Args as SeedBlogDetailsPageArgs } from './seed'
---
+++ src/seed/blog-details-page/seed.ts
@@ -0,0 +1,23 @@
+import { Page } from '@payload-types'
+import { Payload } from 'payload'
+
+import { blogDetailsPageData } from './data'
+
+export interface Args {
+  payload: Payload
+}
+
+const seed = async ({ payload }: Args): Promise<Page> => {
+  try {
+    const result = await payload.create({
+      collection: 'pages',
+      data: blogDetailsPageData,
+    })
+
+    return result
+  } catch (error) {
+    throw error
+  }
+}
+
+export default seed
