0e0b9e39751587093247b7e35bafd0b04ce54492
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-08-20 09:59:45 +0530

    chore: updated trpc seed with comments

--- src/trpc/routers/seed/index.ts
+++ src/trpc/routers/seed/index.ts
@@ -3,6 +3,7 @@ import { getPayloadHMR } from '@payloadcms/next/utilities'
 import { TRPCError } from '@trpc/server'
 
 import { seedAuthorDetailsPage } from '@/seed/author-details-page'
+import { seedAuthors } from '@/seed/authors'
 import { seedAuthorsPage } from '@/seed/authors-page'
 import { seedBlogDetailsPage } from '@/seed/blog-details-page'
 import { seedBlogs } from '@/seed/blogs'
@@ -18,15 +19,19 @@ const payload = await getPayloadHMR({ config: configPromise })
 export const seedRouter = router({
   runSeed: publicProcedure.mutation(async () => {
     try {
+      // Ensure that the seeding functions are called in the correct order.
+      // The blogs seeding depends on tags and authors being seeded first.
+      // Therefore, make sure to seed tags and authors before seeding blogs.
       await seedHomePage()
-      await seedBlogsPage()
-      await seedBlogDetailsPage()
-      await seedBlogs()
       await seedTagsPage()
       await seedTagDetailsPage()
-      await seedTags()
+      await seedTags() // Seed tags first
       await seedAuthorsPage()
       await seedAuthorDetailsPage()
+      await seedAuthors() // Then seed authors
+      await seedBlogsPage()
+      await seedBlogDetailsPage()
+      await seedBlogs() // Finally, seed blogs, which depend on tags and authors
 
       return { success: true }
     } catch (error: any) {
