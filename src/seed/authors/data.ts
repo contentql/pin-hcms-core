d615e3872c391d3302a81a264966d7e791c3605e
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-08-20 14:12:00 +0530

    chore: updated the authors data

--- src/seed/authors/data.ts
+++ src/seed/authors/data.ts
@@ -4,37 +4,37 @@ export type AuthorDataType = RequiredDataFromCollectionSlug<'users'>
 
 export const authorsData: AuthorDataType[] = [
   {
-    name: 'Author 1',
+    username: 'author-1',
     email: 'author.1@gmail.com',
     password: 'Welcome@123',
     role: 'author',
   },
   {
-    name: 'Author 2',
+    username: 'author-2',
     email: 'author.2@gmail.com',
     password: 'Welcome@123',
     role: 'author',
   },
   {
-    name: 'Author 3',
+    username: 'author-3',
     email: 'author.3@gmail.com',
     password: 'Welcome@123',
     role: 'author',
   },
   {
-    name: 'Author 4',
+    username: 'author-4',
     email: 'author.4@gmail.com',
     password: 'Welcome@123',
     role: 'author',
   },
   {
-    name: 'Author 5',
+    username: 'author-5',
     email: 'author.5@gmail.com',
     password: 'Welcome@123',
     role: 'author',
   },
   {
-    name: 'Author 6',
+    username: 'author-6',
     email: 'author.6@gmail.com',
     password: 'Welcome@123',
     role: 'author',
