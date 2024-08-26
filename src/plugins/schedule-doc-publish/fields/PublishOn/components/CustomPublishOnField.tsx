e4b440734a308d5e0ef726d63cedce4e988e7336
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-08-26 12:51:53 +0530

    feat: added timer to the publish on filed

--- src/plugins/schedule-doc-publish/fields/PublishOn/components/CustomPublishOnField.tsx
+++ src/plugins/schedule-doc-publish/fields/PublishOn/components/CustomPublishOnField.tsx
@@ -2,6 +2,7 @@
 
 import { DateTimeField, useFormFields, useFormModified } from '@payloadcms/ui'
 import { DateFieldProps } from 'payload'
+import React, { useEffect, useState } from 'react'
 
 const CustomPublishOnField: React.FC<DateFieldProps> = props => {
   const { fields, dispatch } = useFormFields(([fields, dispatch]) => ({
@@ -13,11 +14,53 @@ const CustomPublishOnField: React.FC<DateFieldProps> = props => {
   const status = fields?._status?.value
   const publishOn = fields?.publishOn?.value
 
+  const [timeRemaining, setTimeRemaining] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (publishOn && status !== 'published') {
+      const targetDate = new Date(String(publishOn)).getTime()
+
+      const updateTimeRemaining = () => {
+        const now = new Date().getTime()
+        const timeDifference = targetDate - now
+
+        if (timeDifference <= 0) {
+          setTimeRemaining('Published')
+        } else {
+          const hours = Math.floor(
+            (timeDifference % (1000 * 3600 * 24)) / (1000 * 3600),
+          )
+          const minutes = Math.floor(
+            (timeDifference % (1000 * 3600)) / (1000 * 60),
+          )
+          const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
+          setTimeRemaining(`${hours}h ${minutes}m ${seconds}s`)
+        }
+      }
+
+      updateTimeRemaining()
+      const interval = setInterval(updateTimeRemaining, 1000)
+
+      return () => clearInterval(interval)
+    } else {
+      setTimeRemaining(null)
+    }
+  }, [publishOn, status])
+
   return (
-    <DateTimeField
-      {...props}
-      readOnly={Boolean(publishOn) && status === 'published' && !isFormModified}
-    />
+    <div>
+      <DateTimeField
+        {...props}
+        readOnly={
+          Boolean(publishOn) && status === 'published' && !isFormModified
+        }
+      />
+      {timeRemaining && (
+        <div className='time-remaining' style={{ paddingTop: '3px' }}>
+          Time Remaining: {timeRemaining}
+        </div>
+      )}
+    </div>
   )
 }
 
